c0bbf7b1b70b7086f894d62488aeb1fb0ee0afa8
Laura Umbelino <lauraoliveiraumbelino22@gmail.com>
2021-11-09 16:51:22 -0300

    Project clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,10 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-
 function App() {
-
         return (
+                <div>
+                        <h1>Ola mundo</h1>
+                </div>
+        )
+}
 
-          <>
-          
-          <NavBar />
-          <div className = "container">
-                  <h1 className ="text-primary py-3">Dashboard de Vnedas</h1>
-
-                  <div className ="row px-3">
-                          <div className ="col-sm-6">
-                                  <h5 className ="text-center text-secondary">Taxa de Sucesso</h5>
-                          </div>
-
-
-                  </div>
-
-          </div>
-           
-
-
-                        )
-   }
+export default App;
 
