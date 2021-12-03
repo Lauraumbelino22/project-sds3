eb5e41d5e5658974e8391e1554c296cc6a1c9ff3
Laura Umbelino <lauraoliveiraumbelino22@gmail.com>
2021-12-03 11:33:59 -0300

    Barchart

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,11 +1,11 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
 
 }
 
-export type SaleSucess = {
-    sellerName : string
+export type SaleSuccess = {
+    sellerName : string;
     visited: number;
     deals: number;
 
